7b70b6a93aa79f75c472d4a4de15633bc8aa2d60
swamy <18pa5a0406@vishnu.edu.in>
2020-03-01 15:03:51 +0530

    initial

---
+++ oop/src/main/java/com/swamy/oop/Chocolate.java
@@ -0,0 +1,11 @@
+package com.swamy.oop;
+
+public class Chocolate extends Sweet{
+	String Name;
+	int cost;
+	float weight;
+	int sweetness;
+	public Chocolate(String Name,int cost,float weight,int sweetness) {
+		super(Name,cost,weight,sweetness);
+	}
+}
